70fa5942440e29d26183e5f132d1450a802e4597
antgit <iljinaa@gmailcom>
2015-12-04 21:17:51 +0200

    SciFi_Fighter_AK5

---
+++ Assets/Scripts/ShipPilot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipPilot : MonoBehaviour {
+
+	private Rigidbody _rb;
+
+	void Start () {
+		_rb = GetComponent<Rigidbody> ();
+	}
+	
+
+	void FixedUpdate () {
+		_rb.AddRelativeForce (Input.GetAxis ("Vertical") * Vector3.forward * Time.deltaTime * 50);
+	}
+}
