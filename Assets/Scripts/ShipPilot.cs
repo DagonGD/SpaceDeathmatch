11a34593b789e852fda45d5aa936e88db46f3947
antgit <iljinaa@gmailcom>
2015-12-04 22:31:42 +0200

    Rotation and colliders

--- Assets/Scripts/ShipPilot.cs
+++ Assets/Scripts/ShipPilot.cs
@@ -11,6 +11,12 @@ public class ShipPilot : MonoBehaviour {
 	
 
 	void FixedUpdate () {
-		_rb.AddRelativeForce (Input.GetAxis ("Vertical") * Vector3.forward * Time.deltaTime * 50);
+		//Ship engins always move forward
+		_rb.AddRelativeForce (Input.GetAxis ("Vertical") * Vector3.forward * 10);
+
+		var direction = Quaternion.Euler (0f, Input.GetAxis ("Horizontal"), 0f);
+		_rb.velocity = direction * _rb.velocity;
+		_rb.MoveRotation(Quaternion.LookRotation(_rb.velocity));
+		Debug.Log (_rb.velocity);
 	}
 }
